2377b12723853f2cf8812a9cbcb37f364d75cd1f
Adam Andrews <andrewsadam07@gmail.com>
2021-12-14 16:08:53 +0000

    Added Hero

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,6 +4,11 @@ function App() {
   return (
     <div className="App">
       <div className = "App__hero">
+        <div className="App__image"/>
+      <div className="App__title">
+        <h1> Hi i’m Adam - a freelance react developer based in oxford </h1>
+        </div>
+
         </div>
     </div>
   );
